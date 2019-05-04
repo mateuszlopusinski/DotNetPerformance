5c886e7b6f4262212c8f4f19e68f108f42729317
Mateusz Lopusinski <mateusz.lopusinski2@gmail.com>
2019-05-04 14:18:03 +0200

    added benchmarking dotnet nuget to projects

--- Core21/Program.cs
+++ Core21/Program.cs
@@ -1,14 +1,47 @@
 ﻿using System;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Columns;
+using BenchmarkDotNet.Attributes.Exporters;
+using BenchmarkDotNet.Attributes.Jobs;
+using BenchmarkDotNet.Running;
 
 namespace Core21
 {
+    [ClrJob(true), CoreJob]
+    [RPlotExporter, RankColumn]
+    public class Md5VsSha256
+    {
+        private SHA256 sha256 = SHA256.Create();
+        private MD5 md5 = MD5.Create();
+        private byte[] data;
+
+        [Params(1000, 10000)]
+        public int N;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            data = new byte[N];
+            new Random(42).NextBytes(data);
+        }
+
+        [Benchmark]
+        public byte[] Sha256() => sha256.ComputeHash(data);
+
+        [Benchmark]
+        public byte[] Md5() => md5.ComputeHash(data);
+    }
+
+
     internal class Program
     {
         #region Private methods
 
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var summary = BenchmarkRunner.Run<Md5VsSha256>();
+            Console.ReadLine();
         }
 
         #endregion
