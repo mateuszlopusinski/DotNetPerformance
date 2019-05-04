e04fd6ed6ee108c7cc684daca360d1c57708eb21
Mateusz Lopusinski <mateusz.lopusinski2@gmail.com>
2019-05-04 22:42:09 +0200

    metrics for 46

--- Framework46/Program.cs
+++ Framework46/Program.cs
@@ -1,53 +1,61 @@
 ﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Reports;
 using BenchmarkDotNet.Running;
+using FrameworkResourceMeter;
 
 namespace Framework46
 {
     public class Program
     {
+        #region Nested classes
 
-        public class Md5VsSha256
+        public class Benchmark
         {
+            #region Private fields
 
             [Params(1000, 10000)]
             public int N;
+
+            #endregion
+            #region Public static methods
+
+            [Benchmark]
+            public static void Loop()
+            {
+                ResourceMeterClass.InvokerResourceMeters();
+                LoopMethod();
+            }
+
+            #endregion
+            #region Public methods
 
             [GlobalSetup]
             public void Setup()
             {
             }
 
-            //[Benchmark]
-            //public byte[] Sha256() => sha256.ComputeHash(data);
-
-            //[Benchmark]
-            //public byte[] Md5() => md5.ComputeHash(data);
-
-
-            [Benchmark]
-            public static int Calculate()
-            {
-                int res = 123;
-                for (int i = 0; i < 10000; i++)
-                {
-                    if (i % 2 == 0)
-                    {
-                        res = res * res;
-                    }
-                    else
-                    {
-                        res = res + 1;
-                    }
-                }
-                return res;
-            }
+            #endregion
         }
 
-        #region Private methods
+        #endregion
+        #region Public static methods
 
         public static void Main(string[] args)
         {
-            var summary = BenchmarkRunner.Run<Md5VsSha256>();
+            Summary summary = BenchmarkRunner.Run<Benchmark>();
+        }
+
+        #endregion
+        #region Private methods
+
+        private static void LoopMethod()
+        {
+            var res = 123;
+            for (var i = 0; i < 1000000; i++)
+                if (i % 2 == 0)
+                    res = res * res;
+                else
+                    res = res + 1;
         }
 
         #endregion
